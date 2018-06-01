168ade608724e7520abfd2d077355060554851a9
daniel9a <andrade.daniel111@gmail.com>
2018-06-01 11:55:44 -0700

    lab5

---
+++ Lab5/part2.cu
@@ -0,0 +1,77 @@
+#include <chrono>
+#include <iostream>
+#include <math.h>
+
+typedef std::chrono::high_resolution_clock Clock;
+#define NUM_THREADS_IN_BLOCK 256
+
+__global__
+void daxpyGPU(int arraySize, float *a, float *b, float *c, float *result)
+{
+  // Insert your code here.
+} // daxpyGPU()
+
+void daxpyCPU(int arraySize, float *a, float *x, float *y, float *result)
+{
+  for(int index = 0; index < arraySize; index++)
+  {
+    result[index] = a[index] * x[index] + y[index];
+  }
+} // daxpyCPU()
+
+int main(void)
+{
+  int arraySize = 1 << 20;
+  float *a, *x, *y;
+  float *cpuResult, *gpuResult;
+
+  cpuResult = new float[arraySize];
+
+  // Allocate unified memory, accessible from CPU or GPU.
+  cudaMallocManaged(&a, arraySize * sizeof(float));
+  cudaMallocManaged(&x, arraySize * sizeof(float));
+  cudaMallocManaged(&y, arraySize * sizeof(float));
+  cudaMallocManaged(&gpuResult, arraySize * sizeof(float));
+
+  // Initialize arrays on the host.
+  for(int index = 0; index < arraySize; index++)
+  {
+    a[index] = 5.0f;
+    x[index] = 10.0f;
+    y[index] = 20.0f;
+  }
+
+  int blockSize = NUM_THREADS_IN_BLOCK;
+  int numBlocks = (arraySize + blockSize - 1) / blockSize;
+
+  auto start = Clock::now();
+  daxpyCPU(arraySize, a, x, y, cpuResult);
+  auto end = Clock::now();
+
+  daxpyGPU<<<numBlocks, blockSize>>>(arraySize, a, x, y, gpuResult);
+
+  // Wait for GPU to finish before accessing values on the host.
+  cudaDeviceSynchronize();
+
+  // Check for errors. All values should be 70.0f.
+  float maxError = 0.0f;
+
+  for (int index = 0; index < arraySize; index++)
+  {
+    maxError = fmax(maxError, fabs(cpuResult[index] - gpuResult[index]));
+  }
+
+  std::cout << "Max error: " << maxError << std::endl
+            << "CPU time: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+            << " microseconds." << std::endl;
+
+  // Free memory.
+  cudaFree(a);
+  cudaFree(x);
+  cudaFree(y);
+  cudaFree(gpuResult);
+  delete(cpuResult);
+
+  return 0;
+} // main()
